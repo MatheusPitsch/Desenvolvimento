751802fd834101d165bed3d19aa26d52915ab234
Matheus <matheuspitschh@gmail.com>
2022-07-21 16:53:30 -0300

    [change] save

---
+++ Dart-master/dartAvancado/solid/exercicios.dart
@@ -0,0 +1,26 @@
+class Cartex {
+  getOrderDetails() {}
+  getCart() {}
+  insertOrderRepository() {}
+  deleteOrderRepository() {}
+  updateOrderRepository() {}
+  selectOrderRepository() {}
+  removeCartRepository() {}
+  saveCartRepository() {}
+}
+
+class Cart {
+  removeCartRepository() {}
+  saveCartRepository() {}
+  getCart() {}
+}
+
+class CartOrder {
+  insertOrderRepository() {}
+  deleteOrderRepository() {}
+  updateOrderRepository() {}
+}
+
+class GetOrder{
+  getOrderDetails() {}
+}
---
+++ Dart-master/dartAvancado/solid/open_closed_principle/exercicio.dart
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+abstract class filme {
+  void genero();
+}
+
+class Vingadores implements filme {
+  @override
+  void genero() {
+    // TODO: implement genero
+  }
+
+}
